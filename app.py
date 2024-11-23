0cf69e2cfee8fdfa2dd466908558cc33b44ad32a
Patrick Otieno <potieno578@gmail.com>
2024-11-24 00:36:34 +0300

    Solved the method not allowed error

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
-from flask import Flask, render_template, url_for, request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-
-# Configure the SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking for performance
 db = SQLAlchemy(app)
 
-# Define the database model
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Primary key column
-    content = db.Column(db.String(200), nullable=False)  # Task content, cannot be empty
-    completed = db.Column(db.Integer, default=0)  # Default completion status is 0 (not completed)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)  # Automatically add creation date
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<Task {self.id}>'
+        return '<Task %r>' % self.id
 
-# Route for the homepage
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)   
+        new_task = Todo(content=task_content)
 
         try:
             db.session.add(new_task)
@@ -33,11 +28,10 @@ def index():
         except:
             return 'There was an issue adding your task'
 
-
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
-    
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -50,9 +44,10 @@ def delete(id):
     except:
         return 'There was a problem deleting that task'
 
-@app.route('/update/<int:id>', methods=['POST','GET'])
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
+
     if request.method == 'POST':
         task.content = request.form['content']
 
@@ -61,15 +56,10 @@ def update(id):
             return redirect('/')
         except:
             return 'There was an issue updating your task'
+
     else:
         return render_template('update.html', task=task)
 
 
-# Entry point for the application
 if __name__ == "__main__":
-    # Create the database and table(s) if they don't already exist
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
-
-
